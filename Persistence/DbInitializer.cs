74c477715bde9eee392550087f36969c23ae5bfa
sherouk22 <sheroukmohy558@gmail.com>
2025-04-13 23:25:23 +0200

    Store.Api

---
+++ Persistence/DbInitializer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Domain.Contracts;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data;
+
+namespace Persistence
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly StoreDbContext _context;
+
+        public DbInitializer(StoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Initialize()
+        {
+            try
+            {
+                // database found or not
+                //if(_context.Database.GetPendingMigrations().Any())
+                //    _context.Database.Migrate();
+
+                if (!_context.ProductTypes.Any())
+                {
+                    var typesData = File.ReadAllText(@"..\Persistence\Data\Seeding\types.json");
+                    var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
+
+                    if (types is not null && types.Any())
+                    {
+                        _context.ProductTypes.AddRange(types);
+                        _context.SaveChanges();
+                    }
+                }
+
+                if (!_context.ProductBrands.Any())
+                {
+                    var brandsData = File.ReadAllText(@"..\Persistence\Data\Seeding\brands.json");
+                    var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+
+                    if (brands is not null && brands.Any())
+                    {
+                        _context.ProductBrands.AddRange(brands);
+                        _context.SaveChanges();
+                    }
+                }
+
+                if (!_context.Products.Any())
+                {
+                    var ProductsData = File.ReadAllText(@"..\Persistence\Data\Seeding\products.json");
+                    var Products = JsonSerializer.Deserialize<List<Product>>(ProductsData);
+
+                    if (Products is not null && Products.Any())
+                    {
+                        _context.Products.AddRange(Products);
+                        _context.SaveChanges();
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
--- Store.Api/Program.cs
+++ Store.Api/Program.cs
@@ -1,4 +1,9 @@
 
+using Domain.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Persistence.Data;
+
 namespace Store.Api
 {
     public class Program
@@ -10,11 +15,20 @@ namespace Store.Api
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddDbContext<StoreDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
+            });
+
+            builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+
+            SeedDb(app);
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -31,6 +45,15 @@ namespace Store.Api
             app.MapControllers();
 
             app.Run();
+        }
+
+         static void SeedDb(WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+
+            var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+
+            dbInitializer.Initialize();
         }
     }
 }
